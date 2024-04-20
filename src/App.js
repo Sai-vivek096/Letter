24a9f5aa9b7c7cce700fd1bd50766a090c2e6ec1
Sai-vivek096 <163561499+Sai-vivek096@users.noreply.github.com>
2024-04-20 12:59:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Letter from './Components/Letter';
+
+function App() {
+  return (
+    <div className="App">
+     <Letter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Letter.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './Letter.css';
+import audioFile from './Letter.mp3';
+
+const Letter = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleEnvelope = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={toggleEnvelope}>
+      <div className="flap"></div>
+      <div className="body"></div>
+      {isOpen && (
+        <div className="letter fullSize">
+          <div className="letter-content">
+            <p>
+              <b>Dear Mamayya and Atta🖤,<br /></b>
+              I am filled with gratitude for the love and care you have showered upon me over these past few days. Thank you from the bottom of my heart for making me feel like a cherished member of the family. Your support and encouragement have meant the world to me, and I am deeply appreciative of everything you have done.
+            </p>
+             <b> @vickyy</b>
+          </div>
+        </div>
+      )}
+      {isOpen && <audio src={audioFile} autoPlay />}
+    </div>
+  );
+};
+
+export default Letter;
